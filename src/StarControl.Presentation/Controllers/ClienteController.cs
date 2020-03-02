c146178ae4656efb31719b0cc02d3ea4b9965103
Luiz Guilherme Bandeira <arkanael@gmail.com>
2020-03-02 08:44:17 -0300

    Adicionado o AdminLTE e criado o formulario de cadastro de cliente.

--- src/StarControl.Presentation/Controllers/ClienteController.cs
+++ src/StarControl.Presentation/Controllers/ClienteController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using starcontrol.Entities;
+using StarControl.Presentation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,31 @@ namespace StarControl.Presentation.Controllers
     public class ClienteController : Controller
     {
         // GET: Cliente
-        public ActionResult Index()
+        public ActionResult Cadastro()
         {
             return View();
+        }
+
+        public JsonResult CadastrarCliente(ClienteCadastroViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    return Json("OK");
+                }
+                catch (Exception erro)
+                {
+                    Response.StatusCode = 500;
+                    return Json(erro.Message);
+                }
+            }
+            else
+            {
+                Response.StatusCode = 400;
+                return Json("erro");
+
+            }
         }
     }
 }
